22d9b8308407374d3a8a12b5355ba1f531f5dfe3
verowangari <verowangari34@gmail.com>
2022-05-04 13:41:52 +0300

    add auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,19 @@ import React,{useContext} from 'react'
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 const Header = () => {
-  let {name}= useContext(AuthContext)
+  let {user}= useContext(AuthContext)
   return (
     <div >
         <Link to="/"  > Home </Link>
         <span>  | </span>
-        <Link to="/login" > Login </Link>
-    {/* <p>Hello, {name}</p> */}
+        {user?(
+          <p> Logout </p>
+        ):(
+<Link to="/login" > Login </Link>
+        )}
+        
+        {user && <p>Hello, {user.username}</p> }
+    
     </div>
   )
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,12 +1,16 @@
 import { createContext,useState ,useEffect } from 'react'
-
+import jwt_decode from "jwt-decode";
+import { useHistory } from 'react-dom'
 const AuthContext = createContext()
 
 export default AuthContext;
 
 export const AuthProvider=({children}) =>{
+    // localStorage.getItem('authTokens')
     let [authTokens,setauthTokens]=useState(null)
     let [user,setUser]=useState(null)
+
+    // let history=useHistory()
     
     let loginUser =async (e)=>{
         e.preventDefault()
@@ -21,7 +25,9 @@ export const AuthProvider=({children}) =>{
         console.log('response:',response)
         if (response.status==200){
             setauthTokens(data)
-            setUser(data.access)
+            setUser(jwt_decode(data.access))
+            // localStorage.setItem('authTokens',JSON.stringify(data))
+            // history.push('/')
 
         }else{
             alert('Something went wrong!')
@@ -30,6 +36,7 @@ export const AuthProvider=({children}) =>{
     }
 
     let contextData={
+        user:user,
         loginUser: loginUser
    }
     return(
